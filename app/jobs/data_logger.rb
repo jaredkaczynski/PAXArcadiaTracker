c4aa77c9c46502329961ae3092e8a0bbab7ec3b0
Jared <razrsword@gmail.com>
2019-03-22 09:43:39 -0400

    Add player model, move routes to set it as home page

--- app/jobs/data_logger.rb
+++ app/jobs/data_logger.rb
@@ -1,3 +1,6 @@
+require 'rubygems'
+require 'nokogiri'
+
 module PAXArcadiaTracker
   class DataLogger
     def start_logging
@@ -5,10 +8,18 @@ module PAXArcadiaTracker
         next_run_time = self.up_to_nearest_5(Time.now)
         loop do
           sleep(next_run_time - Time.now)
-          puts "I'm in a thread!"
+          self.get_stats
         end
       end
     end
+
+    def get_stats
+      Thread.new do
+        uri = URI('http://paxxp.com/leaderboard')
+        page = Net::HTTP.get(uri)
+      end
+    end
+
     def up_to_nearest_5(n)
       return n if n % 5 == 0
       rounded = n.round(-1)
