1481c9d9ac884df5f4c196792d246e972306919e
Jared <razrsword@gmail.com>
2019-03-22 10:48:34 -0400

    Create new player if player doesn't exist

--- app/jobs/data_logger.rb
+++ app/jobs/data_logger.rb
@@ -25,11 +25,21 @@ module PAXArcadiaTracker
         parsed_page = Nokogiri::HTML(page)
         table = parsed_page.css("div#contentHiscores tbody tr")
         table.each do |player|
-          temp_player = Player.new
-          temp_player.time_completed = Time.now
-          temp_player.time_taken = player.css('td')[3].text.delete(" \n")
-          temp_player.username = player.css('td')[1].css('a').text.delete(" \n")
+            #temp_player = Player.new
+            #temp_player.time_completed = Time.now
+            #temp_player.time_taken = player.css('td')[3].text.delete(" \n")
+            #temp_player.username = player.css('td')[1].css('a').text.delete(" \n")
+            #current_table.insert(temp_player)
+            current_table.insert(player)
+            unless @last_table.include? player
+              new_player = Player.new
+              new_player.time_completed = Time.now
+              new_player.time_taken = player.css('td')[3].text.delete(" \n")
+              new_player.username = player.css('td')[1].css('a').text.delete(" \n")
+            end
         end
+        #Reset last to current if there were changes
+        @last_table = current_table
       end
     end
 
