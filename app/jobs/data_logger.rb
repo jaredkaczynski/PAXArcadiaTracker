b1d2b84bc3a45cab61db7b367b7cfc99ac6717ca
Jared <razrsword@gmail.com>
2019-03-22 10:43:04 -0400

    Strip newlines, get player info, add to table

--- app/jobs/data_logger.rb
+++ app/jobs/data_logger.rb
@@ -3,11 +3,15 @@ require 'nokogiri'
 
 module PAXArcadiaTracker
   class DataLogger
+    @last_table = []
+
     def start_logging
       Thread.new do
-        next_run_time = self.up_to_nearest_5(Time.now)
+        next_run_time = self.up_to_nearest_5(Time.now.to_i)
         loop do
-          sleep(next_run_time - Time.now)
+          if (sleep_time = (next_run_time - Time.now.to_i)) > 0
+            sleep(sleep_time)
+          end
           self.get_stats
         end
       end
@@ -15,13 +19,22 @@ module PAXArcadiaTracker
 
     def get_stats
       Thread.new do
-        uri = URI('http://paxxp.com/leaderboard')
+        current_table = []
+        uri = URI('https://secure.runescape.com/m=hiscore_oldschool/overall.ws')
         page = Net::HTTP.get(uri)
+        parsed_page = Nokogiri::HTML(page)
+        table = parsed_page.css("div#contentHiscores tbody tr")
+        table.each do |player|
+          temp_player = Player.new
+          temp_player.time_completed = Time.now
+          temp_player.time_taken = player.css('td')[3].text.delete(" \n")
+          temp_player.username = player.css('td')[1].css('a').text.delete(" \n")
+        end
       end
     end
 
     def up_to_nearest_5(n)
-      return n if n % 5 == 0
+      return n if n % 50 == 0
       rounded = n.round(-1)
       rounded > n ? rounded : rounded + 5
     end
--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,7 @@
 require_relative 'boot'
 
 require 'rails/all'
+require_relative '../app/jobs/data_logger'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -14,5 +15,7 @@ module PAXArcadiaTracker
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    logger = DataLogger.new
+    logger.start_logging
   end
 end
