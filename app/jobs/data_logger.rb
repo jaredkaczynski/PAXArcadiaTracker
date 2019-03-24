37f3724df8240bf935a94fbb884c8a47defdd3e7
Jared <razrsword@gmail.com>
2019-03-23 22:19:33 -0400

    Fixed it, I was doing sleep delays wrong.

--- app/jobs/data_logger.rb
+++ app/jobs/data_logger.rb
@@ -7,8 +7,8 @@ module PAXArcadiaTracker
     def start_logging
       @last_table = []
       Thread.new do
-        next_run_time = self.up_to_nearest_5(Time.now.to_i)
         loop do
+          next_run_time = self.up_to_nearest_5(Time.now.to_i)
           if (sleep_time = (next_run_time - Time.now.to_i)) > 0
             sleep(sleep_time)
           end
@@ -20,25 +20,26 @@ module PAXArcadiaTracker
     def get_stats
       Thread.new do
         current_table = []
+        puts "Opening url"
         uri = URI('https://secure.runescape.com/m=hiscore_oldschool/overall.ws')
         page = Net::HTTP.get(uri)
         parsed_page = Nokogiri::HTML(page)
         table = parsed_page.css("div#contentHiscores tbody tr.personal-hiscores__row")
         table.each do |player|
-            string_player = player.text.tr("\n","")
-            #temp_player = Player.new
-            #temp_player.time_completed = Time.now
-            #temp_player.time_taken = player.css('td')[3].text.delete(" \n")
-            #temp_player.username = player.css('td')[1].css('a').text.delete(" \n")
-            #current_table.insert(temp_player)
-            current_table << string_player
-            unless @last_table.include? string_player
-              new_player = Player.new
-              new_player.time_completed = Time.now
-              new_player.time_taken = player.css('td')[3].text.delete(" \n")
-              new_player.username = player.css('td')[1].css('a').text.delete(" \n")
-              new_player.save
-            end
+          string_player = player.text.tr("\n", "")
+          #temp_player = Player.new
+          #temp_player.time_completed = Time.now
+          #temp_player.time_taken = player.css('td')[3].text.delete(" \n")
+          #temp_player.username = player.css('td')[1].css('a').text.delete(" \n")
+          #current_table.insert(temp_player)
+          current_table << string_player
+          unless @last_table.include? string_player
+            new_player = Player.new
+            new_player.time_completed = Time.now
+            new_player.time_taken = player.css('td')[3].text.delete(" \n")
+            new_player.username = player.css('td')[1].css('a').text.delete(" \n")
+            new_player.save
+          end
         end
         #Reset last to current if there were changes
         @last_table = current_table
@@ -46,9 +47,9 @@ module PAXArcadiaTracker
     end
 
     def up_to_nearest_5(n)
-      return n if n % 50 == 0
-      rounded = n.round(-1)
-      rounded > n ? rounded : rounded + 5
+      increment = 50
+      rounded = n - (n % increment) + increment
+      return rounded
     end
   end
 end
