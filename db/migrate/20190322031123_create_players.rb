4618421946b49958b0a89a136ed542c294310fd4
Jared <razrsword@gmail.com>
2019-03-23 23:05:27 -0400

    add db create script

---
+++ db/migrate/20190322031123_create_players.rb
@@ -0,0 +1,11 @@
+class CreatePlayers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :players do |t|
+      t.string :username
+      t.datetime :time_completed
+      t.time :time_taken
+
+      t.timestamps
+    end
+  end
+end
