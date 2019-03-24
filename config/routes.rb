dda56c6d05604c2928b8de0c10044c9ac53ac62d
Jared <razrsword@gmail.com>
2019-03-24 14:47:53 -0400

    Proper navigation sidebar, beginnings of graphs

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get 'statistics/home'
+  get '/home', to: 'statistics#home'
+  get '/charts', to: 'statistics#charts'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'statistics#home'
 end
